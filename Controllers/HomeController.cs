98f3f7fbdef5d9377855176a8f5d122da26e7d5e
Mounika <mounika.tunga9@gmail.com>
2017-08-09 18:02:05 -0700

    namespace changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-using loginreg.Connectors;
+using wall.Connectors;
 
 namespace wall.Controllers
 {
